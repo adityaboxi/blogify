e07594eba1e453592569d8d48e77f09e453c1e47
Aditya boxi <pier_looper_8@icloud.com>
2026-03-09 21:08:51 +0530

    Update user.js

--- server/routes/user.js
+++ server/routes/user.js
@@ -9,7 +9,8 @@ const COOKIE_OPTIONS = {
   httpOnly: true,
   secure: true,
   sameSite: "none",
-  maxAge: 7 * 24 * 60 * 60 * 1000, // 7 days
+  maxAge: 7 * 24 * 60 * 60 * 1000,
+  path: "/",
 };
 
 // Get current user
